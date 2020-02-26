48059f34f448c36499cc29531b1477bc34eb5ef5
Student <example@example.com>
2020-02-26 13:43:35 -0800

    update to structure of app, added history of moves

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,34 +3,39 @@ import React from 'react';
 
 
 class Board extends React.Component {
+
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+      </div>
+      <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+      </div>
       </div>
     );
   }
 }
+
 
 export default Board;
